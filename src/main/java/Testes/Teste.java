14a683a72af7bc52f30d461dd5fbd4c4f8f8b413
Raí Joia <megac154@gmail.com>
2024-05-16 20:10:20 -0300

    test: add class for tests

--- src/main/java/Testes/Teste.java
+++ src/main/java/Testes/Teste.java
@@ -1,4 +1,52 @@
 package Testes;
 
+import TAD.Pilha;
+
+import java.util.Scanner;
+
 public class Teste {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Pilha lista = new Pilha();
+
+    char opcao;
+    do {
+      System.out.println("\nMenu:");
+      System.out.println("a) Inserir novo nó");
+      System.out.println("b) Remover último nó");
+      System.out.println("c) Mostrar conteúdo do nó atual");
+      System.out.println("d) Mostrar conteúdo do próximo nó");
+      System.out.println("e) Mostrar conteúdo do nó anterior");
+      System.out.println("f) Sair");
+      System.out.print("Escolha uma opção: ");
+      opcao = scanner.next().charAt(0);
+
+      switch (opcao) {
+        case 'a':
+          System.out.print("Digite o item a ser inserido: ");
+          int item = scanner.nextInt();
+          lista.inserir(item);
+          break;
+        case 'b':
+          lista.remover();
+          break;
+        case 'c':
+          lista.mostrarAtual();
+          break;
+        case 'd':
+          lista.mostrarProximo();
+          break;
+        case 'e':
+          lista.mostrarAnterior();
+          break;
+        case 'f':
+          System.out.println("Encerrando o programa...");
+          break;
+        default:
+          System.out.println("Opção inválida!");
+          break;
+      }
+    } while (opcao != 'f');
+    scanner.close();
+  }
 }
