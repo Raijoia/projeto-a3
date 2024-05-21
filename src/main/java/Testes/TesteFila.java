8f996a66f3ee4abbfae40cc065119328419c6432
Raí Joia <megac154@gmail.com>
2024-05-20 21:53:53 -0300

    feat: add test for the Fila in the project

--- src/main/java/Testes/TesteFila.java
+++ src/main/java/Testes/TesteFila.java
@@ -1,2 +1,48 @@
-package Testes;public class TesteFila {
+package Testes;
+
+import TAD.Fila;
+
+import java.util.Scanner;
+
+public class TesteFila {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Fila lista = new Fila();
+
+    char opcao;
+    do {
+      System.out.println("\nMenu:");
+      System.out.println("a) Inserir novo nó");
+      System.out.println("b) Remover primeiro nó da lista");
+      System.out.println("c) Mostrar conteúdo do inicio");
+      System.out.println("d) Mostrar conteúdo do final");
+      System.out.println("f) Sair");
+      System.out.print("Escolha uma opção: ");
+      opcao = scanner.next().charAt(0);
+
+      switch (opcao) {
+        case 'a':
+          System.out.print("Digite o item a ser inserido: ");
+          int item = scanner.nextInt();
+          lista.inserir(item);
+          break;
+        case 'b':
+          lista.remover();
+          break;
+        case 'c':
+          lista.mostrarPrimeiro();
+          break;
+        case 'd':
+          lista.mostrarUltimo();
+          break;
+        case 'f':
+          System.out.println("Encerrando o programa...");
+          break;
+        default:
+          System.out.println("Opção inválida!");
+          break;
+      }
+    } while (opcao != 'f');
+    scanner.close();
+  }
 }
